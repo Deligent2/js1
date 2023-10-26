e700adc39fb3850ae0d7c24c2fae5c1f3cc921dd
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-10-26 03:11:38 +0300

    javas

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-let a =4     
+
+let a = 4
+
+let b = 55
+
+console.log(a+b)
+
+console.log(a/b)
+
+console.log(a%b)
+
+console.log (a*b)
 
 
+let a = 'hello '
 
+let b = 'Ramazan'
 
+console.log(a+b)
 
-console.log(a -b)
+let a = '3'
+
+let b = 'eminem'
+
+console.log(a-b)
+
+let a = 5
+
+let b = '5'
+
+console.log (+b+a)
+
+let a = 2
+
+let b
+
+consol.log (b)
